4512b31512dae4f1a54707e0c6a6cb3a5a49e633
l3ear <89180097+sutharineeritt@users.noreply.github.com>
2022-12-19 16:44:17 +0700

    Add files via upload

---
+++ solution-test-book-M2/book.js
@@ -0,0 +1,32 @@
+const { template } = require('@babel/core')
+//64130500085 Sutharinee Ritthidetch
+
+class Book {
+    constructor(isbn, title = 'untitled', ebook = false) {
+        this.isbn = isbn
+        this.title = title
+        this.authors = []
+        this.ebook = ebook
+    }
+    addAuthor(fn, ln) {
+        let fullname = {firstname: fn, lastname: ln}
+        this.authors.push(fullname)
+        return this.authors.length
+    }
+    getFirstAuthor(){
+        return this.authors?.[0]
+    }
+    getCoAuthors() {
+        return this.authors?.slice(1)
+    }
+    findAuthor(firstname, lastname){
+       return this.authors.find(a => a.firstname.toUpperCase() === firstname && a.lastname.toUpperCase() === lastname
+                                || a.firstname.toLowerCase() === firstname && a.lastname.toLowerCase() === lastname
+                                || a.firstname === firstname && a.lastname === lastname)??undefined
+    }
+    hasEbook(){
+        return this.ebook
+    }
+}
+
+module.exports = Book
