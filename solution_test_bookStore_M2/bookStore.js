7e1edda8e073285fb968d2aff39640c2d57a2d2b
l3ear <89180097+sutharineeritt@users.noreply.github.com>
2022-12-27 20:37:58 +0700

    Update bookStore.js

--- solution_test_bookStore_M2/bookStore.js
+++ solution_test_bookStore_M2/bookStore.js
@@ -23,16 +23,19 @@ class BookStore {
     //stock value in case adding a new book into array successfully
     }
 
-  sellBook(isbn, quantity){
-    const a = this.findBookIndex(isbn)
-    if(a !== -1){
-    if(this.stores[a].stocks < quantity){
-    return 0
-    }
-    this.stores[a].stocks -= quantity
-    return quantity
-    }
-    return -1
-  }
+    sellBook(isbn, quantity) {
+      const bookIndex = this.stores.findIndex(store => store.isbn === isbn)
+      if (bookIndex === -1) {
+        return -1
+      } // no book in store return -1 if have return 0,1,2,3,4,...
+  
+      const book = this.stores[bookIndex];
+      if (book.stocks < quantity) {
+        return 0
+      } // Not enough stocks to sell
+  
+      book.stocks -= quantity
+      return quantity
+    } // Decrease the stocks and return the selling quantity
 }
 module.exports = BookStore
